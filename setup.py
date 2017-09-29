ea7022be6453b4f4dd9d12b173a08fbd653ac12d
Jan Laukemann <hpcv007h@emmy1.rrze.uni-erlangen.de>
2017-09-29 17:34:27 +0200

    updated package_data

--- setup.py
+++ setup.py
@@ -103,9 +103,10 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['package_data.dat'],
-    #},
+    package_data={
+        'osaca': ['data/*.csv', 'README.rst', 'LICENSE'],
+    },
+    include_package_data=True,
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
