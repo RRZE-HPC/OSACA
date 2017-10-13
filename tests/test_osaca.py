0bf2afd73cd384f847749b7407a1d2a627b9456f
Jan Laukemann <ol68umur@stud.informatik.uni-erlangen.de>
2017-10-13 15:43:47 +0200

    added unittest

--- tests/test_osaca.py
+++ tests/test_osaca.py
@@ -16,9 +16,31 @@ class TestOsaca(unittest.TestCase):
         osa = Osaca('IVB', curr_dir+'/testfiles/taxCalc-ivb-iaca', out)
         osa.inspect_with_iaca()
         result = out.getvalue()
-        print(result)
         result = '\n'.join(result.split('\n')[-27:])
-#        print(result)
         with open(curr_dir+'/test_osaca_iaca.out', encoding='utf-8') as f:
+            assertion = f.read()
+        self.assertEqual(assertion, result)
+    
+    # Test ASM file with IACA marker in two lines
+    def testIACAasm1(self):
+        out = StringIO()
+        curr_dir = '/'.join(os.path.realpath(__file__).split('/')[:-1])
+        osa = Osaca('IVB', curr_dir+'/testfiles/taxCalc-ivb-iaca.S', out)
+        osa.inspect_with_iaca()
+        result = out.getvalue()
+        result = '\n'.join(result.split('\n')[-27:])
+        with open(curr_dir+'/test_osaca_iaca_asm.out', encoding='utf-8') as f:
+            assertion = f.read()
+        self.assertEqual(assertion, result)
+
+    # Test ASM file with IACA marker in four lines
+    def testIACAasm2(self):
+        out = StringIO()
+        curr_dir = '/'.join(os.path.realpath(__file__).split('/')[:-1])
+        osa = Osaca('IVB', curr_dir+'/testfiles/taxCalc-ivb-iaca2.S', out)
+        osa.inspect_with_iaca()
+        result = out.getvalue()
+        result = '\n'.join(result.split('\n')[-27:])
+        with open(curr_dir+'/test_osaca_iaca_asm.out', encoding='utf-8') as f:
             assertion = f.read()
         self.assertEqual(assertion, result)
