ba06552eef2973dfa061aa3739677f197ea19520
Jan Laukemann <ol68umur@stud.informatik.uni-erlangen.de>
2017-11-09 20:32:26 +0100

    initial upload

---
+++ examples/scale.c
@@ -0,0 +1,13 @@
+
+void scale(int N){
+    void dummy(double*, double*);
+    double a[N], b[N];
+    double s;
+    
+    //STARTLOOP
+    for(int i=0; i<N; ++i){
+            a[i] = s * b[i];
+    }
+
+    dummy(&a[1],&b[1]);
+}
---
+++ examples/triad.c
@@ -0,0 +1,12 @@
+
+void triad(int N){
+    void dummy(dobule*);
+    double a[N], b[N], c[N], d[N];
+    double s;
+
+    //STARTLOOP
+    for(int i=0; i<N; ++i)
+        a[i] = b[i] + c[i] * d[i];
+    
+    dummy(&a[1]);
+}
