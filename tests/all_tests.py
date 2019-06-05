119dc5baa981d4774e873de5d4bf8a6f36346fe4
JanLJL <jan.laukemann@fau.de>
2019-06-05 13:51:41 +0200

    refactoring to modulize marker_utils

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -8,7 +8,7 @@ suite = unittest.TestLoader().loadTestsFromNames(
     [
         'test_parser_x86att',
         'test_parser_AArch64v81',
-        'test_analyzer'
+        'test_marker_utils'
     ]
 )
 
