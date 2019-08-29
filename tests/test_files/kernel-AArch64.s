ff672fb5ec2a2f0440b338b130b90850e7e1ae06
JanLJL <jan.laukemann@fau.de>
2019-08-29 17:52:27 +0200

    more instructions

--- tests/test_files/kernel-AArch64.s
+++ tests/test_files/kernel-AArch64.s
@@ -17,4 +17,8 @@
     stp q2, q3, [x10]
     add x10, x10, #64           // =64
     adds    x12, x12, #1            // =1
+    fmov    s0, -1.0e+0
+    fmov    s1, #2.0f
+    prfm    pldl1keep, [x26, #2112]
+
     b.ne    .LBB0_32
