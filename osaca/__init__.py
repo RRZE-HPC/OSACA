9ea2c5f46dab50e5a5d63457b428c7d0ba8ec917
JanLJL <jan.laukemann@fau.de>
2019-11-18 23:00:07 +0100

    version bump

--- osaca/__init__.py
+++ osaca/__init__.py
@@ -1,6 +1,6 @@
 """Open Source Architecture Code Analyzer"""
 name = 'osaca'
-__version__ = '0.3.1.dev1'
+__version__ = '0.3.1'
 
 # To trigger travis deployment to pypi, do the following:
 # 1. Increment __version___
