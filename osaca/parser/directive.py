317816b9d3ff81786dae4f05fc01b6c57208846b
stefandesouza <stefan.desouza@outlook.com>
2023-08-20 12:10:07 +0200

    Separate operand files with inheritance, str and repr classes

--- osaca/parser/directive.py
+++ osaca/parser/directive.py
@@ -4,14 +4,9 @@ from osaca.parser.operand import Operand
 
 class DirectiveOperand(Operand):
     def __init__(self, NAME_ID = None, PARAMETER_ID = None, COMMENT_ID = None):
-        super().__init__()
-        self._NAME_ID = NAME_ID
+        super().__init__(NAME_ID)
         self._PARAMETER_ID = PARAMETER_ID
         self._COMMENT_ID = COMMENT_ID
-    
-    @property
-    def name(self):
-        return self._NAME_ID
     
     @property
     def parameters(self):
@@ -29,10 +24,6 @@ class DirectiveOperand(Operand):
             raise StopIteration
         return self._COMMENT_ID.pop(0)
 
-    @name.setter
-    def name(self, name):
-        self._NAME_ID = name
-    
     @parameters.setter
     def parameters(self, parameters):
         self._PARAMETER_ID = parameters
@@ -40,3 +31,9 @@ class DirectiveOperand(Operand):
     @comment.setter
     def comment(self, comment):
         self._COMMENT_ID = comment
+
+    def __str__(self):
+        return f"Directive(NAME_ID={self._NAME_ID}, PARAMETERS={self._PARAMETER_ID}, COMMENT={self._COMMENT_ID})"
+
+    def __repr__(self):
+        return f"DirectiveOperand(NAME_ID={self._NAME_ID}, PARAMETERS={self._PARAMETER_ID}, COMMENT={self._COMMENT_ID})"
--- osaca/parser/label.py
+++ osaca/parser/label.py
@@ -4,17 +4,8 @@ from osaca.parser.operand import Operand
 
 class LabelOperand(Operand):
     def __init__(self, NAME_ID = None, COMMENT_ID = None):
-        super().__init__()
-        self._NAME_ID = NAME_ID
+        super().__init__(NAME_ID)
         self._COMMENT_ID = COMMENT_ID
-    
-    @property
-    def name(self):
-        return self._NAME_ID
-    
-    @name.setter
-    def name(self, name):
-        self._NAME_ID = name
 
     @property
     def comment(self):
@@ -31,4 +22,10 @@ class LabelOperand(Operand):
         if not self._COMMENT_ID:
             raise StopIteration
         return self._COMMENT_ID.pop(0)
+
+    def __str__(self):
+        return f"LabelOperand(NAME_ID={self._NAME_ID}, COMMENT={self._COMMENT_ID})"
+
+    def __repr__(self):
+        return f"LabelOperand(NAME_ID={self._NAME_ID}, COMMENT={self._COMMENT_ID})"
     
