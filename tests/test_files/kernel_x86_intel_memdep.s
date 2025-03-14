7930e4d704ac0d9461bc2653d77362eb911d5003
JanLJL <jan.laukemann@fau.de>
2025-03-14 15:40:16 +0100

    take +- operator of offset/index in mem-addr into account

--- tests/test_files/kernel_x86_intel_memdep.s
+++ tests/test_files/kernel_x86_intel_memdep.s
@@ -1,15 +1,15 @@
 ; Translated from kernel_x86_memdep.s
 L4:
-  vmovsd [rax+8], xmm0
-  add rax, 8
-  vmovsd [rax+rcx*8+8], xmm0
-  vaddsd xmm0, xmm0, [rax]
-  sub rax, -8
-  vaddsd xmm0, xmm0, [rax-8]
-  dec rcx
-  vaddsd xmm0, xmm0, [rax+rcx*8+8]
-  mov rdx, rcx
-  vaddsd xmm0, xmm0, [rax+rdx*8+8]
+  vmovsd [rax+8], xmm0              # line 3          <---------------------------------+
+  add rax, 8                        #  rax=rax_orig+8                                   |
+  vmovsd [rax+rcx*8+8], xmm0        # line 5          <------------------------------------------+
+  vaddsd xmm0, xmm0, [rax]          # depends on line 3, rax+8;[rax] == [rax+8] --------+        |
+  sub rax, -8                       #  rax=rax_orig+16                                  |        |
+  vaddsd xmm0, xmm0, [rax-8]        # depends on line 3, rax+16;[rax-8] == [rax+8] -----+        |
+  dec rcx                           #  rcx=rcx_orig-1                                            |
+  vaddsd xmm0, xmm0, [rax+rcx*8+8]  # depends on line 5, [(rax+8)+(rcx-1)*8+8] == [rax+rcx*+8] --+
+  mov rdx, rcx                      #                                                            |
+  vaddsd xmm0, xmm0, [rax+rdx*8+8]  # depends on line 5, rcx == rdx -----------------------------+
   vmulsd xmm0, xmm0, xmm1
   add rax, 8
   cmp rsi, rax
